9011688ee3fe54eff3caa1cdf70cd4acd340b3d2
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-08 00:25:50 +0100

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,16 @@
+#!/usr/bin/python3
 """Generate Pascal's Triangle"""
+
 
 def pascal_triangle(n):
     """Generate Pascal's Triangle"""
-
+    result = []
+    for i in range(0, n):
+        val = 1
+        row = []
+        for j in range(0, i):
+            row.append(int(val))
+            val = val * (i - j) / (j + 1)
+        row.append(1)
+        result.append(row)
+    return result
