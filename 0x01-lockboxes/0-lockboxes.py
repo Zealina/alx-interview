4b10bc41c6732047f67e586a5c153bacd050f071
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-14 23:17:36 +0100

    Return

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,21 +8,23 @@ from collections import deque
 def canUnlockAll(boxes):
     """Check to see if you can open all boxes
     Return: True if all boxes can open, false otherwise"""
-    if not boxes:
+    if not boxes or not boxes[0]:
         return False
     keys = deque(boxes[0])
     visited = {0}
     boxes_length = len(boxes)
-    un_length = 0
+    length = 0
     while keys:
+        length += 1
         key = keys.popleft()
-        if key in visited:
-            continue
-        if key >= boxes_length:
-            un_length += 1
+        if key in visited or key >= boxes_length:
             continue
         keys.extend(boxes[key])
         visited.add(key)
-    if len(visited) == boxes_length + un_length:
+    total_len = 0
+    for i in boxes:
+        total_len += len(i)
+
+    if total_len == length:
         return True
     return False
