e7a2372723dc77c9219dc09d40d5655ad2dc5fc8
Zealina Hudson <zealinahudson@gmail.com>
2024-05-02 08:39:36 +0100

    Dynamic Programming

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Dynamic Programming: Coin chnage problem"""
+
+def makeChange(coins, total):
+    """Return the minimum amount of coins to make total"""
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for c in coins:
+            if i - c >= 0:
+                dp[i] = min(dp[i], 1 + dp[i - c])
+    return dp[total] if dp[total] != total + 1 else -1
